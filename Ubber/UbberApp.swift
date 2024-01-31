93bec1dd446f8fa6dc6854174097339d16066ddf
Sugawara61 <107302679+Sugawara61@users.noreply.github.com>
2024-01-31 20:12:10 +0900

    Initial Commit

---
+++ Ubber/UbberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbberApp.swift
+//  Ubber
+//
+//  Created by 菅原隆太 on 2024/01/31.
+//
+
+import SwiftUI
+
+@main
+struct UbberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
