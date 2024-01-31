14e3dbeb6de3f1750ec521687705ab083cf1d179
Sugawara61 <107302679+Sugawara61@users.noreply.github.com>
2024-01-31 20:36:02 +0900

    Commit Test

--- Ubber/ContentView.swift
+++ Ubber/ContentView.swift
@@ -17,6 +17,13 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hello, world!")
             Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
         }
         .padding()
     }
